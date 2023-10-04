4ac2c38d54cecb3bdbd6ebb8cc5e5a97a3afc03d
hakonstrom <hakon.s.strom@nmbu.no>
2023-10-04 18:29:34 +0200

    Done with assignment 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,47 +9,9 @@ import { useState } from "react";
 
 export default function Home() {
 
-    const [products, setProducts] = useState(Array.from(createProducts({ count: 10, faker }).values()))
-
-    let cart: object[] = []
-    let totalSum  = 0
-    const addToCartHandler = (id: number, title: string, price: number) => {
-        cart = [...cart, {id, title, price}]
-        console.log(cart)
-        totalSum = addToTotalSumCart(cart)
-        console.log("Total sum: " + totalSum)
-    }
-
-    const addToTotalSumCart = (cart: object[]) => {
-        let sum = 0
-        for (const iterator of cart) {
-            sum+=iterator.price
-        }
-        return sum
-    }
-
-    const removeFromCartHandler = (id: number) => {
-        //cart = cart.filter((product) => product.id != id)
-        let index = -1
-        index = cart.findIndex((object) => object.id == id)
-
-        console.log(index)
-        if (index != -1) {
-        cart.splice(index, 1)
-        }
-        console.log(cart)
-        totalSum = addToTotalSumCart(cart)
-        console.log("Total sum: " + totalSum)
-    }
-
 
     return (
         <div className="flex flex-row">
-            <Products>
-                {products.map((product) => (
-                    <ProductItem key={product.id}{...product} addToCart={addToCartHandler} removeFromCart={removeFromCartHandler} />
-                ))}
-            </Products>
-            <ShoppingCart />
+            <p>Her var det lite</p>
         </div>)
 }
